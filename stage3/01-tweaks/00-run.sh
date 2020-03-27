7802e7b6b35047ea8b64b655e687d319ffe9812a
David <david@opensignups.com>
2020-03-27 14:04:29 -0400

    chromium kiosk mode at startup

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,3 +1,21 @@
 #!/bin/bash -e
 
 rm -f "${ROOTFS_DIR}/etc/systemd/system/dhcpcd.service.d/wait.conf"
+
+# create an autostart file that runs chromium and points to our kiosk url
+sed "s|RACETIMING_KIOSK_URL|$RACETIMING_KIOSK_URL|" < files/autostart > /tmp/autostart
+install -v -d ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/
+install -v -m 644 /tmp/autostart ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/
+rm /tmp/autostart
+
+# not sure if this is necessary, but putting the wpa_supplicant.conf file in /boot
+# overcomes rkfill
+install -v -m 644  ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/boot
+
+# make our user sudo-able, probably not necessary, just convenient
+echo "${FIRST_USER_NAME} ALL=(ALL) NOPASSWD: ALL" > "${ROOTFS_DIR}/etc/sudoers.d/010_${FIRST_USER_NAME}-nopasswd"
+
+# autologin - normally this is in stage 4 but bare-bones kiosk mode doesn't go that far
+on_chroot << EOF
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_behaviour B4
+EOF
